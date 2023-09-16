66e76576d43581ed6da2291f7f7028e03f074f82
Kunal89204 <kunalkhandelwal108@gmail.com>
2023-09-16 13:23:27 +0530

    Theme Changed

--- berry.js
+++ berry.js
@@ -1,20 +1,21 @@
 
-// for (let i = 0; i < array.length; i++) {
-//     const element = array[i];
-    
-// }
-fetch(`https://pokeapi.co/api/v2/berry-flavor/1`)
+// for (let i = 0; i < 100; i++) {
+
+    fetch(`https://pokeapi.co/api/v2/berry/1`)
 
 .then((respo) =>{
     return respo.json()
 }).then((data) => {
-    // console.log(`Name: ${data.name}`);
-    // console.log(`ID: ${data.id}`);
-    // console.log(`Growth Time: ${data.growth_time} hours`);
-    // console.log(`Max Harvest: ${data.max_harvest} times`);
-    // console.log(`Size: ${data.size}`);
-    // console.log(`Smoothness: ${data.smoothness}`);
-    // console.log(`Soil Dryness: ${data.soil_dryness}`);
-    // console.log(`Firmness: ${data.firmness.name}`);
-    console.log(`Potency: ${data.berries.potency}`);
+    console.log(`Name: ${data.name}`);
+    console.log(`ID: ${data.id}`);
+    console.log(`Growth Time: ${data.growth_time} hours`);
+    console.log(`Max Harvest: ${data.max_harvest} times`);
+    console.log(`Size: ${data.size}`);
+    console.log(`Smoothness: ${data.smoothness}`);
+    console.log(`Soil Dryness: ${data.soil_dryness}`);
+    console.log(`Firmness: ${data.firmness.name}`);
+    console.log(`Item: ${data.item.name}`);
+    console.log(`Item: ${data.natural_gift_type.name}`);
 })
+    
+// }
