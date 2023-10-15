d6c8b2b2b6bfd1d22f066d7c582e836587de2ea8
Kunal89204 <kunalkhandelwal108@gmail.com>
2023-10-15 10:45:48 +0530

    added search functionality

--- fetch_poke.js
+++ fetch_poke.js
@@ -1,75 +1,54 @@
-for (let i = 1; i <= 500; i++) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-      .then(function (respo) {
-        return respo.json();
-      })
-      .then(function (data) {
-        const container = document.querySelector(".container");
+for (let i = 1; i <= 50; i++) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+    .then(function (respo) {
+      return respo.json();
+    })
+    .then(function (data) {
+      const container = document.querySelector(".container");
 
-        let pkmn = document.createElement("div");
-        pkmn.classList.add("pokemon");
-        let image = document.createElement("div");
-        image.classList.add("img");
+      let pkmn = document.createElement("div");
+      pkmn.classList.add("pokemon");
+      let image = document.createElement("div");
+      image.classList.add("img");
 
-        let img = document.createElement("img");
-        pkmn.appendChild(image);
-        image.appendChild(img);
+      let img = document.createElement("img");
+      pkmn.appendChild(image);
+      image.appendChild(img);
 
-        let norm = document.createElement("div");
-        norm.classList.add("norm");
-        let shiny = document.createElement("div");
-        shiny.classList.add("shiny");
+      let norm = document.createElement("div");
+      norm.classList.add("norm");
+      let shiny = document.createElement("div");
+      shiny.classList.add("shiny");
 
-        image.appendChild(norm);
-        image.appendChild(shiny);
+      image.appendChild(norm);
+      image.appendChild(shiny);
 
-        let id = document.createElement("p");
-        id.classList.add("id");
-        let name = document.createElement("p");
-        name.classList.add("name");
-        let height = document.createElement("p");
-        height.classList.add("height");
-        let weight = document.createElement("p");
-        weight.classList.add("weight");
+      let id = document.createElement("p");
+      id.classList.add("id");
+      let name = document.createElement("p");
+      name.classList.add("name");
+      let height = document.createElement("p");
+      height.classList.add("height");
+      let weight = document.createElement("p");
+      weight.classList.add("weight");
 
-        pkmn.appendChild(id);
-        pkmn.appendChild(name);
-        pkmn.appendChild(height);
-        pkmn.appendChild(weight);
+      pkmn.appendChild(id);
+      pkmn.appendChild(name);
+      pkmn.appendChild(height);
+      pkmn.appendChild(weight);
 
-        container.appendChild(pkmn);
+      container.appendChild(pkmn);
 
-        // Calling Values from the api
+      // Calling Values from the api
 
+      img.src = data.sprites.front_default;
+      id.textContent = `Id: ${data.id}`;
+      name.textContent = `Name: ${data.name.toUpperCase()}`;
+      height.textContent = `Height: ${data.height}`;
+      weight.textContent = `Width: ${data.weight}`;
+
+      norm.addEventListener("click", function () {
         img.src = data.sprites.front_default;
-        id.textContent = `Id: ${data.id}`;
-        name.textContent = `Name: ${data.name.toUpperCase()}`;
-        height.textContent = `Height: ${data.height}`;
-        weight.textContent = `Width: ${data.weight}`;
-
-        norm.addEventListener("click", function () {
-          img.src = data.sprites.front_default;
-
-          img.addEventListener("mouseover", function () {
-            img.src = data.sprites.back_default;
-          });
-
-          img.addEventListener("mouseout", function () {
-            img.src = data.sprites.front_default;
-          });
-        });
-
-        shiny.addEventListener("click", function () {
-          img.src = data.sprites.front_shiny;
-
-          img.addEventListener("mouseover", function () {
-            img.src = data.sprites.back_shiny;
-          });
-
-          img.addEventListener("mouseout", function () {
-            img.src = data.sprites.front_shiny;
-          });
-        });
 
         img.addEventListener("mouseover", function () {
           img.src = data.sprites.back_default;
@@ -78,8 +57,63 @@ for (let i = 1; i <= 500; i++) {
         img.addEventListener("mouseout", function () {
           img.src = data.sprites.front_default;
         });
-      })
-      .catch((error) => {
-        console.log("error", error);
       });
-  }
+
+      shiny.addEventListener("click", function () {
+        img.src = data.sprites.front_shiny;
+
+        img.addEventListener("mouseover", function () {
+          img.src = data.sprites.back_shiny;
+        });
+
+        img.addEventListener("mouseout", function () {
+          img.src = data.sprites.front_shiny;
+        });
+      });
+
+      img.addEventListener("mouseover", function () {
+        img.src = data.sprites.back_default;
+      });
+
+      img.addEventListener("mouseout", function () {
+        img.src = data.sprites.front_default;
+      });
+
+
+      // Search function
+
+      let search = document.getElementById("search")
+      let pokemons = document.querySelectorAll(".pokemon")
+
+      search.addEventListener("input", function () {
+        pokemons.forEach(pokemon => {
+          let input = search.value.toLowerCase();
+          let name = pokemon.querySelector(".name").textContent
+          if (name.toLowerCase().includes(input)) {
+            console.log(pokemon.querySelector(".name").textContent)
+            pokemon.style.display = "block"
+            pokemon.style.height = "fit-content"
+          } else {
+            pokemon.style.display = "none"
+
+          }
+
+
+        })
+
+
+
+      })
+
+
+
+
+    })
+    .catch((error) => {
+      console.log("error", error);
+    });
+
+
+}
+
+
